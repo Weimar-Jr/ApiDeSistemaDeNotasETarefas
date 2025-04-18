00e9bf5b0e547c4e1861fafc4cdfc399770b151b
Weimar Jr <weimarvlademirojunior@gmail.com>
2025-04-18 17:52:06 -0400

    Primeira commit com o projeto no zero

---
+++ src/main/java/com/empresax/ApiDeNotasETarefas/ApiDeNotasETarefasApplication.java
@@ -0,0 +1,13 @@
+package com.empresax.ApiDeNotasETarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeNotasETarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeNotasETarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresax/ApiDeNotasETarefas/ApiDeNotasETarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresax.ApiDeNotasETarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeNotasETarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
